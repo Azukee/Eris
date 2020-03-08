54c863e3913a7f68b15e4b52340562ebcf3c80fa
Azukee <theorybreakerhacks@gmail.com>
2020-03-08 20:16:09 +0100

    Implement ClientStatPacket

---
+++ ErisLib/Server/Packets/Client/ClientStatPacket.cs
@@ -0,0 +1,24 @@
+using ErisLib.Server.Packets.Models;
+
+namespace ErisLib.Server.Packets.Client
+{
+    public class ClientStatPacket : Packet
+    {
+        public string Name;
+        public int Value;
+
+        public override PacketType Type => PacketType.CLIENTSTAT;
+
+        public override void Read(PacketReader r)
+        {
+            Name = r.ReadString();
+            Value = r.ReadInt32();
+        }
+
+        public override void Write(PacketWriter w)
+        {
+            w.Write(Name);
+            w.Write(Value);
+        }
+    }
+}
