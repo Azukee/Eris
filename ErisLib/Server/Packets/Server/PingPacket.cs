1260b9ecb8e8651144ad06631492f0e13bb61771
Azukee <theorybreakerhacks@gmail.com>
2020-03-08 20:01:40 +0100

    Braindead moment

--- ErisLib/Server/Packets/Server/PingPacket.cs
+++ ErisLib/Server/Packets/Server/PingPacket.cs
@@ -1,9 +1,13 @@
+using ErisLib.Server.Packets.Models;
+
 namespace ErisLib.Server.Packets.Server
 {
     public class PingPacket : Packet
     {
         public int Serial;
 
+        public override PacketType Type => PacketType.PING;
+        
         public override void Read(PacketReader r) => Serial = r.ReadInt32();
         
         public override void Write(PacketWriter w) => w.Write(Serial);
