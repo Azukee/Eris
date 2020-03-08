c99f0c269a91358719ee11f9778f16652f63a18c
Azukee <theorybreakerhacks@gmail.com>
2020-03-08 20:08:54 +0100

    Implement ShowEffectPacket

---
+++ ErisLib/Server/Packets/Server/ShowEffectPacket.cs
@@ -0,0 +1,37 @@
+using ErisLib.Game;
+using ErisLib.Server.Packets.Models;
+
+namespace ErisLib.Server.Packets.Server
+{
+    public class ShowEffectPacket : Packet
+    {
+        public EffectType EffectType;
+        public int TargetId;
+        public Location PosA;
+        public Location PosB;
+        public ARGB Color;
+        public double Duration;
+
+        public override PacketType Type => PacketType.SHOWEFFECT;
+
+        public override void Read(PacketReader r)
+        {
+            EffectType = (EffectType)r.ReadByte();
+            TargetId = r.ReadInt32();
+            PosA =(Location) new Location().Read(r);
+            PosB = (Location) new Location().Read(r);
+            Color = ARGB.Read(r);
+            Duration = r.ReadSingle();
+        }
+
+        public override void Write(PacketWriter w)
+        {
+            w.Write((byte)EffectType);
+            w.Write(TargetId);
+            PosA.Write(w);
+            PosB.Write(w);
+            Color.Write(w);
+            w.Write(Duration);
+        }
+    }
+}
