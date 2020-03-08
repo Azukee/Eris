890f16374709b32e3f742569bf4d0227902d182e
Azukee <theorybreakerhacks@gmail.com>
2020-03-08 19:53:54 +0100

    Implement NewTickPacket

---
+++ ErisLib/Server/Packets/Server/NewTickPacket.cs
@@ -0,0 +1,31 @@
+namespace ErisLib.Server.Packets.Server
+{
+    public class NewTickPacket : Packet
+    {
+        public int TickId;
+        public int TickTime;
+        public Status[] Statuses;
+
+        public override PacketType Type
+        { get { return PacketType.NEWTICK; } }
+
+        public override void Read(PacketReader r)
+        {
+            TickId = r.ReadInt32();
+            TickTime = r.ReadInt32();
+
+            Statuses = new Status[r.ReadInt16()];
+            for (int i = 0; i < Statuses.Length; i++)
+                Statuses[i] = (Status)new Status().Read(r);
+        }
+
+        public override void Write(PacketWriter w)
+        {
+            w.Write(TickId);
+            w.Write(TickTime);
+
+            w.Write((short)Statuses.Length);
+            foreach (Status s in Statuses) s.Write(w);
+        }
+    }
+}
