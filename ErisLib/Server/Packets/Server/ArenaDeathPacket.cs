dcfba723f749f108aea00b5bd59bcac472aa92fd
Azukee <theorybreakerhacks@gmail.com>
2020-03-11 13:54:13 +0100

    Implement ArenaDeathPacket

---
+++ ErisLib/Server/Packets/Server/ArenaDeathPacket.cs
@@ -0,0 +1,15 @@
+﻿using ErisLib.Server.Packets.Models;
+
+namespace ErisLib.Server.Packets.Server
+{
+    public class ArenaDeathPacket : Packet
+    {
+        public int RestartPrice;
+
+        public override PacketType Type => PacketType.ARENADEATH;
+
+        public override void Read(PacketReader r) => RestartPrice = r.ReadInt32();
+
+        public override void Write(PacketWriter w) => w.Write(RestartPrice);
+    }
+}
