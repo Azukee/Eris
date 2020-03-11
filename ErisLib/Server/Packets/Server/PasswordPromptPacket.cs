1cd05545adcb0856eb34761d5a0045f273f910c6
Azukee <theorybreakerhacks@gmail.com>
2020-03-11 13:55:28 +0100

    Implement PasswordPromptPacket

---
+++ ErisLib/Server/Packets/Server/PasswordPromptPacket.cs
@@ -0,0 +1,15 @@
+﻿using ErisLib.Server.Packets.Models;
+
+namespace ErisLib.Server.Packets.Server
+{
+    public class PasswordPromptPacket : Packet
+    {
+		public int CleanPasswordStatus;
+
+        public override PacketType Type => PacketType.PASSWORDPROMPT;
+
+        public override void Read(PacketReader r) => CleanPasswordStatus = r.ReadInt32();
+
+        public override void Write(PacketWriter w) => w.Write(CleanPasswordStatus);
+    }
+}
