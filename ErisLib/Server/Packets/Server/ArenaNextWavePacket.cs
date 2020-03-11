5367492575cd8214401691841a210b550360ba3f
Azukee <theorybreakerhacks@gmail.com>
2020-03-11 13:54:20 +0100

    Implement ArenaNextWavePacket

---
+++ ErisLib/Server/Packets/Server/ArenaNextWavePacket.cs
@@ -0,0 +1,15 @@
+﻿using ErisLib.Server.Packets.Models;
+
+namespace ErisLib.Server.Packets.Server
+{
+    public class ArenaNextWavePacket : Packet
+    {
+        public int TypeId;
+
+        public override PacketType Type => PacketType.ARENANEXTWAVE;
+
+        public override void Read(PacketReader r) => TypeId = r.ReadInt32();
+
+        public override void Write(PacketWriter w) => w.Write(TypeId);
+    }
+}
