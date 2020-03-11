b06ab5a64f8e30dc9a5755898257f8d409607dfb
Azukee <theorybreakerhacks@gmail.com>
2020-03-11 13:40:17 +0100

    Implement TinkerQuestPacket

---
+++ ErisLib/Server/Packets/Client/TinkerQuestPacket.cs
@@ -0,0 +1,15 @@
+﻿using ErisLib.Server.Packets.Models;
+
+namespace ErisLib.Server.Packets.Client
+{
+    public class TinkerQuestPacket : Packet
+    {
+        public Slot Slot;
+
+        public override PacketType Type => PacketType.TINKERQUEST;
+
+        public override void Read(PacketReader r) => Slot = new Slot().Read(r);
+
+        public override void Write(PacketWriter w) => Slot.Write(w);
+    }
+}
