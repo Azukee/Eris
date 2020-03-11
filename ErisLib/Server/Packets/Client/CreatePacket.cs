0b1a38d5175ce07b723a81ba1309e51f79c8628f
Azukee <theorybreakerhacks@gmail.com>
2020-03-11 13:37:23 +0100

    Implement CreatePacket

---
+++ ErisLib/Server/Packets/Client/CreatePacket.cs
@@ -0,0 +1,24 @@
+﻿using ErisLib.Server.Packets.Models;
+
+namespace ErisLib.Server.Packets.Client
+{
+    public class CreatePacket : Packet
+    {
+        public ushort ClassType;
+        public ushort SkinType;
+
+        public override PacketType Type => PacketType.CREATE;
+
+        public override void Read(PacketReader r)
+        {
+            ClassType = r.ReadUInt16();
+            SkinType = r.ReadUInt16();
+        }
+
+        public override void Write(PacketWriter w)
+        {
+            w.Write((ushort)ClassType);
+            w.Write((ushort)SkinType);
+        }
+    }
+}
