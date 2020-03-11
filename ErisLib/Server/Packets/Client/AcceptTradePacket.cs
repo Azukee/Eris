bbfc9aff9d88de5b014c4246463c952860a7ee0d
Azukee <theorybreakerhacks@gmail.com>
2020-03-11 13:35:49 +0100

    Implement AcceptTradePacket

---
+++ ErisLib/Server/Packets/Client/AcceptTradePacket.cs
@@ -0,0 +1,33 @@
+﻿using ErisLib.Server.Packets.Models;
+
+namespace ErisLib.Server.Packets.Client
+{
+    public class AcceptTradePacket : Packet
+    {
+        public bool[] MyOffers;
+        public bool[] YourOffers;
+
+        public override PacketType Type => PacketType.ACCEPTTRADE;
+
+        public override void Read(PacketReader r)
+        {
+            MyOffers = new bool[r.ReadInt16()];
+            for (int i = 0; i < MyOffers.Length; i++)
+                MyOffers[i] = r.ReadBoolean();
+
+            YourOffers = new bool[r.ReadInt16()];
+            for (int i = 0; i < YourOffers.Length; i++)
+                YourOffers[i] = r.ReadBoolean();
+        }
+
+        public override void Write(PacketWriter w)
+        {
+            w.Write((ushort)MyOffers.Length);
+            foreach (bool i in MyOffers)
+                w.Write(i);
+            w.Write((ushort)YourOffers.Length);
+            foreach (bool i in YourOffers)
+                w.Write(i);
+        }
+    }
+}
