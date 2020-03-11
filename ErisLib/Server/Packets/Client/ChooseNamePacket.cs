3244e3029d9d0291ef2157b2be16da776d63123f
Azukee <theorybreakerhacks@gmail.com>
2020-03-11 13:37:08 +0100

    Implement ChooseNamePacket

---
+++ ErisLib/Server/Packets/Client/ChooseNamePacket.cs
@@ -0,0 +1,15 @@
+﻿using ErisLib.Server.Packets.Models;
+
+namespace ErisLib.Server.Packets.Client
+{
+    public class ChooseNamePacket : Packet
+    {
+        public string Name;
+
+        public override PacketType Type => PacketType.CHOOSENAME;
+
+        public override void Read(PacketReader r) => Name = r.ReadString();
+
+        public override void Write(PacketWriter w) => w.Write(Name);
+    }
+}
