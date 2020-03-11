5f0a6f3cf7dd4c6b9ba01790906d2aa528a2363d
Azukee <theorybreakerhacks@gmail.com>
2020-03-11 13:56:59 +0100

    Implement TradeRequestedPacket

---
+++ ErisLib/Server/Packets/Server/TradeRequestedPacket.cs
@@ -0,0 +1,15 @@
+﻿using ErisLib.Server.Packets.Models;
+
+namespace ErisLib.Server.Packets.Server
+{
+    public class TradeRequestedPacket : Packet
+    {
+        public string Name;
+
+        public override PacketType Type => PacketType.TRADEREQUESTED;
+
+        public override void Read(PacketReader r) => Name = r.ReadString();
+
+        public override void Write(PacketWriter w) => w.Write(Name);
+    }
+}
