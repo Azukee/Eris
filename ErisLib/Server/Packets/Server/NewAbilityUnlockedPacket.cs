116bfffabfdb57420165f1132e74572280eca176
Azukee <theorybreakerhacks@gmail.com>
2020-03-11 13:55:23 +0100

    Implement NewAbilityUnlockedPacket

---
+++ ErisLib/Server/Packets/Server/NewAbilityUnlockedPacket.cs
@@ -0,0 +1,16 @@
+﻿using ErisLib.Game;
+using ErisLib.Server.Packets.Models;
+
+namespace ErisLib.Server.Packets.Server
+{
+    public class NewAbilityUnlockedPacket : Packet
+    {
+		public Ability AbilityType;
+
+        public override PacketType Type => PacketType.NEWABILITYUNLOCKED;
+
+        public override void Read(PacketReader r) => AbilityType = (Ability)r.ReadInt32();
+
+        public override void Write(PacketWriter w) => w.Write((int)Type);
+    }
+}
