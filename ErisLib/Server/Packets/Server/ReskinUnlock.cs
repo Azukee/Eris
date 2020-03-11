704f08c40cdb74582543544e67d1beae0d8b9064
Azukee <theorybreakerhacks@gmail.com>
2020-03-11 13:56:13 +0100

    Implement ReskinUnlock

---
+++ ErisLib/Server/Packets/Server/ReskinUnlock.cs
@@ -0,0 +1,24 @@
+﻿using ErisLib.Server.Packets.Models;
+
+namespace ErisLib.Server.Packets.Server
+{
+	public class ReskinUnlock : Packet
+	{
+		public int SkinId;
+        public int IsPetSkin;
+
+		public override PacketType Type => PacketType.RESKINUNLOCK;
+
+        public override void Read(PacketReader r)
+		{
+			SkinId = r.ReadInt32();
+            IsPetSkin = r.ReadInt32();
+        }
+
+		public override void Write(PacketWriter w)
+		{
+			w.Write(SkinId);
+            w.Write(IsPetSkin);
+		}
+	}
+}
