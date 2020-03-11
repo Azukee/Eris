8ecc0cc1cbfe4ccecdd21c03e43a7c54966dd23e
Azukee <theorybreakerhacks@gmail.com>
2020-03-11 13:39:58 +0100

    Implement SetConditionPacket

---
+++ ErisLib/Server/Packets/Client/SetConditionPacket.cs
@@ -0,0 +1,24 @@
+﻿using ErisLib.Server.Packets.Models;
+
+namespace ErisLib.Server.Packets.Client
+{
+    public class SetConditionPacket : Packet
+    {
+        public byte ConditionEffect;
+        public float ConditionDuration;
+
+        public override PacketType Type => PacketType.SETCONDITION;
+
+        public override void Read(PacketReader r)
+        {
+            ConditionEffect = r.ReadByte();
+            ConditionDuration = r.ReadSingle();
+        }
+
+        public override void Write(PacketWriter w)
+        {
+            w.Write(ConditionEffect);
+            w.Write(ConditionDuration);
+        }
+    }
+}
