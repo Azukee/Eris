faed64350961ab019123fc07d456abf7e15a628a
Azukee <theorybreakerhacks@gmail.com>
2020-03-11 13:55:05 +0100

    Implement InvitedToGuildPacket

---
+++ ErisLib/Server/Packets/Server/InvitedToGuildPacket.cs
@@ -0,0 +1,23 @@
+﻿using ErisLib.Server.Packets.Models;
+
+namespace ErisLib.Server.Packets.Server
+{
+    public class InvitedToGuildPacket : Packet
+    {
+		public string Name;
+		public string GuildName;
+        public override PacketType Type => PacketType.INVITEDTOGUILD;
+
+        public override void Read(PacketReader r)
+        {
+            Name = r.ReadString();
+            GuildName = r.ReadString();
+        }
+
+        public override void Write(PacketWriter w)
+        {
+            w.Write(Name);
+            w.Write(GuildName);
+        }
+    }
+}
