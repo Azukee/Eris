54271fb4c862649cc280de7b4a015cefb480cafc
Azukee <theorybreakerhacks@gmail.com>
2020-03-11 13:55:39 +0100

    Implement PicPacket

---
+++ ErisLib/Server/Packets/Server/PicPacket.cs
@@ -0,0 +1,15 @@
+﻿using ErisLib.Server.Packets.Models;
+
+namespace ErisLib.Server.Packets.Server
+{
+    public class PicPacket : Packet
+    {
+        public Bitmap BitmapData;
+
+        public override PacketType Type => PacketType.PIC;
+
+        public override void Read(PacketReader r) => BitmapData = BitmapData.Read(r);
+
+        public override void Write(PacketWriter w) => BitmapData.Write(w);
+    }
+}
