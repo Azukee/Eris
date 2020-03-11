50a9c0c52c8fcb647c65199cf8f39fb81dfb2207
Azukee <theorybreakerhacks@gmail.com>
2020-03-11 13:37:44 +0100

    Implement EscapePacket

---
+++ ErisLib/Server/Packets/Client/EscapePacket.cs
@@ -0,0 +1,15 @@
+﻿using ErisLib.Server.Packets.Models;
+
+namespace ErisLib.Server.Packets.Client
+{
+    public class EscapePacket : Packet
+    {
+        public override PacketType Type => PacketType.ESCAPE;
+
+        public override void Read(PacketReader r)
+        { }
+
+        public override void Write(PacketWriter w)
+        { }
+    }
+}
