2fb75de876a497111916470b6b8063aaf3c9616f
Azukee <theorybreakerhacks@gmail.com>
2020-03-11 13:57:34 +0100

    Implement VerifyEmailDialogPacket

---
+++ ErisLib/Server/Packets/Server/VerifyEmailDialogPacket.cs
@@ -0,0 +1,15 @@
+﻿using ErisLib.Server.Packets.Models;
+
+namespace ErisLib.Server.Packets.Server
+{
+    public class VerifyEmailDialogPacket : Packet
+    {
+        public override PacketType Type => PacketType.VERIFYEMAILDIALOG;
+
+        public override void Read(PacketReader r)
+        { }
+
+        public override void Write(PacketWriter w)
+        { }
+    }
+}
