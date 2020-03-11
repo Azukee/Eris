c64d5a771ffaa1dec0d77ff47395336a15afadf2
Azukee <theorybreakerhacks@gmail.com>
2020-03-11 13:54:55 +0100

    Implement HatchEggPacket

---
+++ ErisLib/Server/Packets/Server/HatchEggPacket.cs
@@ -0,0 +1,23 @@
+﻿using ErisLib.Server.Packets.Models;
+
+namespace ErisLib.Server.Packets.Server
+{
+    public class HatchEggPacket : Packet
+    {
+        public string PetName;
+        public int PetSkinId;
+        public override PacketType Type => PacketType.HATCHEGG;
+
+        public override void Read(PacketReader r)
+        {
+            PetName = r.ReadString();
+            PetSkinId = r.ReadInt32();
+        }
+
+        public override void Write(PacketWriter w)
+        {
+            w.Write(PetName);
+            w.Write(PetSkinId);
+        }
+    }
+}
