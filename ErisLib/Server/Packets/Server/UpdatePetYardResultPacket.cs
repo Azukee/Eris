1f37aab80401a773504a022628a001fd60759d37
Azukee <theorybreakerhacks@gmail.com>
2020-03-11 13:57:26 +0100

    Implement UpdatePetYardResultPacket

---
+++ ErisLib/Server/Packets/Server/UpdatePetYardResultPacket.cs
@@ -0,0 +1,15 @@
+﻿using ErisLib.Server.Packets.Models;
+
+namespace ErisLib.Server.Packets.Server
+{
+    public class UpdatePetYardResultPacket : Packet
+    {
+        public int TypeId;
+
+        public override PacketType Type => PacketType.UPGRADEPETYARDRESULT;
+
+        public override void Read(PacketReader r) => TypeId = r.ReadInt32();
+
+        public override void Write(PacketWriter w) => w.Write(TypeId);
+    }
+}
