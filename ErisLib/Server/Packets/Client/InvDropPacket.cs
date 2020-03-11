c3100331ff07c1c2ac21a0d0f270e6f098a440c3
Azukee <theorybreakerhacks@gmail.com>
2020-03-11 13:38:17 +0100

    Implement InvDropPacket

---
+++ ErisLib/Server/Packets/Client/InvDropPacket.cs
@@ -0,0 +1,15 @@
+﻿using ErisLib.Server.Packets.Models;
+
+namespace ErisLib.Server.Packets.Client
+{
+    public class InvDropPacket : Packet
+    {
+        public Slot Slot;
+
+        public override PacketType Type => PacketType.INVDROP;
+
+        public override void Read(PacketReader r) => Slot = new Slot().Read(r);
+
+        public override void Write(PacketWriter w) => Slot.Write(w);
+    }
+}
