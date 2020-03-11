757ea2d60fc3b895b423d62da8a564ac55262155
Azukee <theorybreakerhacks@gmail.com>
2020-03-11 13:37:38 +0100

    Implement EnterArenaPacket

---
+++ ErisLib/Server/Packets/Client/EnterArenaPacket.cs
@@ -0,0 +1,15 @@
+﻿using ErisLib.Server.Packets.Models;
+
+namespace ErisLib.Server.Packets.Client
+{
+    public class EnterArenaPacket : Packet
+    {
+        public int Currency;
+
+        public override PacketType Type => PacketType.ENTERARENA;
+
+        public override void Read(PacketReader r) => Currency = r.ReadInt32();
+
+        public override void Write(PacketWriter w) => w.Write(Currency);
+    }
+}
