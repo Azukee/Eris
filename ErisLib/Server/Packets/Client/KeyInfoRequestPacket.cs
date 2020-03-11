8cf3607eaf3973528d3b2b3c1ed80f2f27758656
Azukee <theorybreakerhacks@gmail.com>
2020-03-11 13:38:32 +0100

    Implement KeyInfoRequestPacket

---
+++ ErisLib/Server/Packets/Client/KeyInfoRequestPacket.cs
@@ -0,0 +1,15 @@
+﻿using ErisLib.Server.Packets.Models;
+
+namespace ErisLib.Server.Packets.Client
+{
+    public class KeyInfoRequestPacket : Packet
+    {
+        public int ItemType;
+
+        public override PacketType Type => PacketType.KEYINFOREQUEST;
+
+        public override void Read(PacketReader r) => ItemType = r.ReadInt32();
+
+        public override void Write(PacketWriter w) => w.Write(ItemType);
+    }
+}
