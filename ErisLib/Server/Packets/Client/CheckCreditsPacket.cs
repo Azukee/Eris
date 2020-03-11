5f937d131a79a0520765eee2a1a67172f4a80cb1
Azukee <theorybreakerhacks@gmail.com>
2020-03-11 13:37:00 +0100

    Implement CheckCreditsPacket

---
+++ ErisLib/Server/Packets/Client/CheckCreditsPacket.cs
@@ -0,0 +1,15 @@
+﻿using ErisLib.Server.Packets.Models;
+
+namespace ErisLib.Server.Packets.Client
+{
+    public class CheckCreditsPacket : Packet
+    {
+        public override PacketType Type => PacketType.CHECKCREDITS;
+
+        public override void Read(PacketReader r)
+        { }
+
+        public override void Write(PacketWriter w)
+        { }
+    }
+}
