32bd4425a0420b0f0724d79218a73a6a96e98d74
Azukee <theorybreakerhacks@gmail.com>
2020-03-11 13:38:00 +0100

    Implement GuildInvitePacket

---
+++ ErisLib/Server/Packets/Client/GuildInvitePacket.cs
@@ -0,0 +1,15 @@
+﻿using ErisLib.Server.Packets.Models;
+
+namespace ErisLib.Server.Packets.Client
+{
+    public class GuildInvitePacket : Packet
+    {
+        public string Name;
+
+        public override PacketType Type => PacketType.GUILDINVITE;
+
+        public override void Read(PacketReader r) => Name = r.ReadString();
+
+        public override void Write(PacketWriter w) => w.Write(Name);
+    }
+}
