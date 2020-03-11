a571fb9425a383da3351d7baa49a2a54958f1076
Azukee <theorybreakerhacks@gmail.com>
2020-03-11 13:54:50 +0100

    Implement FilePacket

---
+++ ErisLib/Server/Packets/Server/FilePacket.cs
@@ -0,0 +1,25 @@
+﻿using ErisLib.Server.Packets.Models;
+
+namespace ErisLib.Server.Packets.Server
+{
+    public class FilePacket : Packet
+    {
+        public string Name;
+        public byte[] Bytes;
+        public override PacketType Type => PacketType.FILE;
+
+        public override void Read(PacketReader r)
+        {
+            Name = r.ReadString();
+            Bytes = new byte[r.ReadInt32()];
+            Bytes = r.ReadBytes(Bytes.Length);
+        }
+
+        public override void Write(PacketWriter w)
+        {
+            w.Write(Name);
+            w.Write(Bytes.Length);
+            w.Write(Bytes);
+        }
+    }
+}
