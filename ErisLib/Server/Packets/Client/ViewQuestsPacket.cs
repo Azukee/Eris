4cb4185fdf75d2ddb774359d59ce708a49c94d3b
Azukee <theorybreakerhacks@gmail.com>
2020-03-11 13:40:23 +0100

    Implement ViewQuestsPacket

---
+++ ErisLib/Server/Packets/Client/ViewQuestsPacket.cs
@@ -0,0 +1,15 @@
+﻿using ErisLib.Server.Packets.Models;
+
+namespace ErisLib.Server.Packets.Client
+{
+    public class ViewQuestsPacket : Packet
+    {
+        public override PacketType Type => PacketType.VIEWQUESTS;
+
+        public override void Read(PacketReader r)
+        { }
+
+        public override void Write(PacketWriter w)
+        { }
+    }
+}
