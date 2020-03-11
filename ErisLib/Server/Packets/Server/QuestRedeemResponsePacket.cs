4e7482fd705b71860ee63fe9de530a2a7204c06e
Azukee <theorybreakerhacks@gmail.com>
2020-03-11 13:56:00 +0100

    Implement QuestRedeemResponsePacket

---
+++ ErisLib/Server/Packets/Server/QuestRedeemResponsePacket.cs
@@ -0,0 +1,24 @@
+﻿using ErisLib.Server.Packets.Models;
+
+namespace ErisLib.Server.Packets.Server
+{
+    public class QuestRedeemResponsePacket : Packet
+    {
+        public bool Success;
+        public string Message;
+
+        public override PacketType Type => PacketType.QUESTREDEEMRESPONSE;
+
+        public override void Read(PacketReader r)
+        {
+            Success = r.ReadBoolean();
+            Message = r.ReadString();
+        }
+
+        public override void Write(PacketWriter w)
+        {
+            w.Write(Success);
+            w.Write(Message);
+        }
+    }
+}
