c967bf3bcd9192434b00fe0a418f7785b1b7fc9b
Azukee <theorybreakerhacks@gmail.com>
2020-03-11 13:38:23 +0100

    Implement JoinGuildPacket

---
+++ ErisLib/Server/Packets/Client/JoinGuildPacket.cs
@@ -0,0 +1,15 @@
+﻿using ErisLib.Server.Packets.Models;
+
+namespace ErisLib.Server.Packets.Client
+{
+    public class JoinGuildPacket : Packet
+    {
+        public string GuildName;
+
+        public override PacketType Type => PacketType.JOINGUILD;
+
+        public override void Read(PacketReader r) => GuildName = r.ReadString();
+
+        public override void Write(PacketWriter w) => w.Write(GuildName);
+    }
+}
