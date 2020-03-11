9de289ad26c293a8cdf252ed016cfe3e536ed5c1
Azukee <theorybreakerhacks@gmail.com>
2020-03-11 13:38:40 +0100

    Implement LeaveArenaPacket

---
+++ ErisLib/Server/Packets/Client/LeaveArenaPacket.cs
@@ -0,0 +1,15 @@
+﻿using ErisLib.Server.Packets.Models;
+
+namespace ErisLib.Server.Packets.Client
+{
+    public class LeaveArenaPacket : Packet
+    {
+        public int Time;
+
+        public override PacketType Type => PacketType.LEAVEARENA;
+
+        public override void Read(PacketReader r) => Time = r.ReadInt32();
+
+        public override void Write(PacketWriter w) => w.Write(Time);
+    }
+}
