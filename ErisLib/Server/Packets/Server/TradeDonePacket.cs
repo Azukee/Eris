395d5dbd31ff815a6f9758eae7081badc5234ecb
Azukee <theorybreakerhacks@gmail.com>
2020-03-11 13:56:54 +0100

    Implement TradeDonePacket

---
+++ ErisLib/Server/Packets/Server/TradeDonePacket.cs
@@ -0,0 +1,28 @@
+﻿using ErisLib.Server.Packets.Models;
+
+namespace ErisLib.Server.Packets.Server
+{
+    public class TradeDonePacket : Packet
+    {
+		/*
+		TradeSuccessful = 0
+		PlayerCanceled = 1
+		*/
+
+        public int Result;
+        public string Message;
+        public override PacketType Type => PacketType.TRADEDONE;
+
+        public override void Read(PacketReader r)
+        {
+            Result = r.ReadInt32();
+            Message = r.ReadString();
+        }
+
+        public override void Write(PacketWriter w)
+        {
+            w.Write(Result);
+            w.Write(Message);
+        }
+    }
+}
