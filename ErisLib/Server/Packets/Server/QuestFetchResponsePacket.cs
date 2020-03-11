e45052aa6bcd6dd321ecd064ae618963c2d62fd1
Azukee <theorybreakerhacks@gmail.com>
2020-03-11 13:55:52 +0100

    Implement QuestFetchResponsePacket

---
+++ ErisLib/Server/Packets/Server/QuestFetchResponsePacket.cs
@@ -0,0 +1,29 @@
+﻿using ErisLib.Server.Packets.Models;
+
+namespace ErisLib.Server.Packets.Server
+{
+    public class QuestFetchResponsePacket : Packet
+    {
+        public int Tier;
+        public string Goal;
+        public string Description;
+        public string Image;
+        public override PacketType Type => PacketType.QUESTFETCHRESPONSE;
+
+        public override void Read(PacketReader r)
+        {
+            Tier = r.ReadInt32();
+            Goal = r.ReadString();
+            Description = r.ReadString();
+            Image = r.ReadString();
+        }
+
+        public override void Write(PacketWriter w)
+        {
+            w.Write(Tier);
+            w.Write(Goal);
+            w.Write(Description);
+            w.Write(Image);
+        }
+    }
+}
