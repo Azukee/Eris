60ac650d9afdf6e9c1c0a8d42d897cff8ec233ed
Azukee <theorybreakerhacks@gmail.com>
2020-03-11 13:55:34 +0100

    Implement PetEvolveResultPacket

---
+++ ErisLib/Server/Packets/Server/PetEvolveResultPacket.cs
@@ -0,0 +1,26 @@
+﻿using ErisLib.Server.Packets.Models;
+
+namespace ErisLib.Server.Packets.Server
+{
+    public class PetEvolveResultPacket : Packet
+    {
+        public int PetId;
+        public int InitialSkin;
+        public int FinalSkin;
+        public override PacketType Type => PacketType.EVOLVEPET;
+
+        public override void Read(PacketReader r)
+        {
+            PetId = r.ReadInt32();
+            InitialSkin = r.ReadInt32();
+            FinalSkin = r.ReadInt32();
+        }
+
+        public override void Write(PacketWriter w)
+        {
+            w.Write(PetId);
+            w.Write(InitialSkin);
+            w.Write(FinalSkin);
+        }
+    }
+}
