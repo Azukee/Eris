a440806240f77d2d89796e7c68d8483f8c5e1638
Azukee <theorybreakerhacks@gmail.com>
2020-03-11 13:36:15 +0100

    Implement BuyPacket

---
+++ ErisLib/Server/Packets/Client/BuyPacket.cs
@@ -0,0 +1,24 @@
+﻿using ErisLib.Server.Packets.Models;
+
+namespace ErisLib.Server.Packets.Client
+{
+    public class BuyPacket : Packet
+    {
+        public int ObjectId;
+		public int Quantity;
+
+        public override PacketType Type => PacketType.BUY;
+
+        public override void Read(PacketReader r)
+        {
+            ObjectId = r.ReadInt32();
+			Quantity = r.ReadInt32();
+        }
+
+        public override void Write(PacketWriter w)
+        {
+            w.Write(ObjectId);
+			w.Write(Quantity);
+        }
+    }
+}
