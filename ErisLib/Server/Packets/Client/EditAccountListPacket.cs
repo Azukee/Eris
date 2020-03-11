30d6d08be32d8d4b509017b937e30453ba9bb333
Azukee <theorybreakerhacks@gmail.com>
2020-03-11 13:37:31 +0100

    Implement EditAccountListPacket

---
+++ ErisLib/Server/Packets/Client/EditAccountListPacket.cs
@@ -0,0 +1,27 @@
+﻿using ErisLib.Server.Packets.Models;
+
+namespace ErisLib.Server.Packets.Client
+{
+    public class EditAccountListPacket : Packet
+    {
+        public int AccountListId;
+        public bool Add;
+        public int ObjectId;
+
+        public override PacketType Type => PacketType.EDITACCOUNTLIST;
+
+        public override void Read(PacketReader r)
+        {
+            AccountListId = r.ReadInt32();
+            Add = r.ReadBoolean();
+            ObjectId = r.ReadInt32();
+        }
+
+        public override void Write(PacketWriter w)
+        {
+            w.Write(AccountListId);
+            w.Write(Add);
+            w.Write(ObjectId);
+        }
+    }
+}
