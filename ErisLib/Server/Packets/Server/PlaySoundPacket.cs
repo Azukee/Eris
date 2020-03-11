3d012a1ddb4143e00f6cca136d3644a092cc3eb1
Azukee <theorybreakerhacks@gmail.com>
2020-03-11 13:55:44 +0100

    Implement PlaySoundPacket

---
+++ ErisLib/Server/Packets/Server/PlaySoundPacket.cs
@@ -0,0 +1,24 @@
+﻿using ErisLib.Server.Packets.Models;
+
+namespace ErisLib.Server.Packets.Server
+{
+    public class PlaySoundPacket : Packet
+    {
+        public int OwnerId;
+        public int SoundId;
+
+        public override PacketType Type => PacketType.PLAYSOUND;
+
+        public override void Read(PacketReader r)
+        {
+            OwnerId = r.ReadInt32();
+            SoundId = r.ReadByte();
+        }
+
+        public override void Write(PacketWriter w)
+        {
+            w.Write(OwnerId);
+            w.Write((byte)SoundId);
+        }
+    }
+}
