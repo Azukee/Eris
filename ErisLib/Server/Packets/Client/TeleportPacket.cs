b8369b15f685cac5f445fe3cbfb693a74b06b5a7
Azukee <theorybreakerhacks@gmail.com>
2020-03-11 13:40:11 +0100

    Implement TeleportPacket

---
+++ ErisLib/Server/Packets/Client/TeleportPacket.cs
@@ -0,0 +1,15 @@
+﻿using ErisLib.Server.Packets.Models;
+
+namespace ErisLib.Server.Packets.Client
+{
+    public class TeleportPacket : Packet
+    {
+        public int ObjectId;
+
+        public override PacketType Type => PacketType.TELEPORT;
+
+        public override void Read(PacketReader r) => ObjectId = r.ReadInt32();
+
+        public override void Write(PacketWriter w) => w.Write(ObjectId);
+    }
+}
