e2a707f5353b816fb64be93f837b6d6058d30a69
Azukee <theorybreakerhacks@gmail.com>
2020-03-11 13:38:46 +0100

    Implement PetCommandPacket

---
+++ ErisLib/Server/Packets/Client/PetCommandPacket.cs
@@ -0,0 +1,24 @@
+﻿using ErisLib.Server.Packets.Models;
+
+namespace ErisLib.Server.Packets.Client
+{
+    public class PetCommandPacket : Packet
+    {
+        public int CommandId;
+        public uint PetId;
+
+        public override PacketType Type => PacketType.PETCOMMAND;
+
+        public override void Read(PacketReader r)
+        {
+            CommandId = (int)r.ReadByte();
+            PetId = (uint)r.ReadInt32();
+        }
+
+        public override void Write(PacketWriter w)
+        {
+            w.Write((byte)CommandId);
+            w.Write((int)PetId);
+        }
+    }
+}
