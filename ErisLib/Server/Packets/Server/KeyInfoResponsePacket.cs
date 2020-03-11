315a9afef5fbee3488fd9eb4ccb0ec6f60949639
Azukee <theorybreakerhacks@gmail.com>
2020-03-11 13:55:11 +0100

    Implement KeyInfoResponsePacket

---
+++ ErisLib/Server/Packets/Server/KeyInfoResponsePacket.cs
@@ -0,0 +1,27 @@
+﻿using ErisLib.Server.Packets.Models;
+
+namespace ErisLib.Server.Packets.Server
+{
+	public class KeyInfoResponsePacket : Packet
+    {
+        public string Name;
+        public string Description;
+        public string Creator;
+
+		public override PacketType Type => PacketType.KEYINFORESPONSE;
+
+        public override void Read(PacketReader r)
+        {
+            Name = r.ReadString();
+            Description = r.ReadString();
+            Creator = r.ReadString();
+        }
+
+		public override void Write(PacketWriter w)
+		{
+			w.Write(Name);
+            w.Write(Description);
+            w.Write(Creator);
+		}
+	}
+}
