5c6ec8b592eb581701ffcf60fbc7532fbd475e2b
Azukee <theorybreakerhacks@gmail.com>
2020-03-11 13:56:07 +0100

    Implement RemovePetFromListPacket

---
+++ ErisLib/Server/Packets/Server/RemovePetFromListPacket.cs
@@ -0,0 +1,21 @@
+﻿using ErisLib.Server.Packets.Models;
+
+namespace ErisLib.Server.Packets.Server
+{
+    public class RemovePetFromListPacket : Packet
+    {
+        public int PetId;
+
+        public override PacketType Type => PacketType.REMOVEPET;
+
+        public override void Read(PacketReader r)
+        {
+            PetId = r.ReadInt32();
+        }
+
+        public override void Write(PacketWriter w)
+        {
+            w.Write(PetId);
+        }
+    }
+}
