e78aa418b856c092cd67ecbe60957ca515d57c35
Azukee <theorybreakerhacks@gmail.com>
2020-03-11 13:36:29 +0100

    Implement ChangeGuildRankPacket

---
+++ ErisLib/Server/Packets/Client/ChangeGuildRankPacket.cs
@@ -0,0 +1,24 @@
+﻿using ErisLib.Server.Packets.Models;
+
+namespace ErisLib.Server.Packets.Client
+{
+    public class ChangeGuildRankPacket : Packet
+    {
+        public string Name;
+        public int GuildRank;
+
+        public override PacketType Type => PacketType.CHANGEGUILDRANK;
+
+        public override void Read(PacketReader r)
+        {
+            Name = r.ReadString();
+            GuildRank = r.ReadInt32();
+        }
+
+        public override void Write(PacketWriter w)
+        {
+            w.Write(Name);
+            w.Write(GuildRank);
+        }
+    }
+}
