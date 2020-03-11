e037fb8329970e67b09786c18f81215d9e203693
Azukee <theorybreakerhacks@gmail.com>
2020-03-11 13:39:41 +0100

    Implement ReskinPetPacket

---
+++ ErisLib/Server/Packets/Client/ReskinPetPacket.cs
@@ -0,0 +1,27 @@
+﻿using ErisLib.Server.Packets.Models;
+
+namespace ErisLib.Server.Packets.Client
+{
+    public class ReskinPetPacket : Packet
+    {
+        public int PetId;
+		public int NewPetType;
+		public Slot Item;
+
+        public override PacketType Type => PacketType.RESKINPET;
+
+        public override void Read(PacketReader r)
+        {
+            PetId = r.ReadInt32();
+			NewPetType = r.ReadInt32();
+			Item = new Slot().Read(r);
+		}
+
+        public override void Write(PacketWriter w)
+        {
+            w.Write(PetId);
+			w.Write(NewPetType);
+			Item.Write(w);
+        }
+    }
+}
