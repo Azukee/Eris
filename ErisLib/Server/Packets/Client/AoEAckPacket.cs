5bf164ca4f0da2d80d6e0b2f88b758d3b4d36786
Azukee <theorybreakerhacks@gmail.com>
2020-03-11 13:36:09 +0100

    Implement AoEAckPacket

---
+++ ErisLib/Server/Packets/Client/AoEAckPacket.cs
@@ -0,0 +1,24 @@
+﻿using ErisLib.Server.Packets.Models;
+
+namespace ErisLib.Server.Packets.Client
+{
+    public class AoEAckPacket : Packet
+    {
+        public int Time;
+        public Location Position;
+
+        public override PacketType Type => PacketType.AOEACK;
+
+        public override void Read(PacketReader r)
+        {
+            Time = r.ReadInt32();
+            Position = new Location().Read(r);
+        }
+
+        public override void Write(PacketWriter w)
+        {
+            w.Write(Time);
+            Position.Write(w);
+        }
+    }
+}
