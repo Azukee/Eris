9e9704d5d6c6ddd5d044c2293121dd894a415968
Azukee <theorybreakerhacks@gmail.com>
2020-03-11 13:54:45 +0100

    Implement DeathPacket

---
+++ ErisLib/Server/Packets/Server/DeathPacket.cs
@@ -0,0 +1,33 @@
+﻿using ErisLib.Server.Packets.Models;
+
+namespace ErisLib.Server.Packets.Server
+{
+    public class DeathPacket : Packet
+    {
+        public string AccountId;
+        public int CharId;
+        public string KilledBy;
+        public int ZombieType;
+        public int ZombieId;
+
+        public override PacketType Type => PacketType.DEATH;
+
+        public override void Read(PacketReader r)
+        {
+            AccountId = r.ReadString();
+            CharId = r.ReadInt32();
+            KilledBy = r.ReadString();
+            ZombieType = r.ReadInt32();
+            ZombieId = r.ReadInt32();
+        }
+
+        public override void Write(PacketWriter w)
+        {
+            w.Write(AccountId);
+            w.Write(CharId);
+            w.Write(KilledBy);
+            w.Write(ZombieType);
+            w.Write(ZombieId);
+        }
+    }
+}
