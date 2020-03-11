0db153a96fd94a2233b8f793d55ad283a4812b25
Azukee <theorybreakerhacks@gmail.com>
2020-03-11 13:39:35 +0100

    Implement ReskinPacket

---
+++ ErisLib/Server/Packets/Client/ReskinPacket.cs
@@ -0,0 +1,21 @@
+﻿using ErisLib.Server.Packets.Models;
+
+namespace ErisLib.Server.Packets.Client
+{
+    public class ReskinPacket : Packet
+    {
+        public int SkinId;
+
+        public override PacketType Type => PacketType.RESKIN;
+
+        public override void Read(PacketReader r)
+        {
+            SkinId = r.ReadInt32();
+        }
+
+        public override void Write(PacketWriter w)
+        {
+            w.Write(SkinId);
+        }
+    }
+}
